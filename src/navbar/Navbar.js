f426ae43f60f2df62b70390d152231b44cc9e5c2
ssook1222 <chlthr1222@naver.com>
2022-01-16 16:59:17 +0900

    ADD : Navbar 기능 구현

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import"./Navbar.css"
+import {Navbar, Nav, NavDropdown, Container, Image} from "react-bootstrap";
+class Navbars extends React.Component {
+    render() {
+        return(
+            <div>
+                <Navbar id="navbar" className="background" style={{marginBottom:"20px"}} expand="lg">
+                    <Container>
+                        <Navbar.Brand style={{color:"white"}} href="#home">
+                            <Image style={{width:"15%"}} src="/images/Main-Logo.png"></Image>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ms-auto justify-content-end">
+                                <Nav.Link style={{color:"white", marginRight:"10px",fontSize:"20px"}} href="#home">수강신청 주의사항</Nav.Link>
+                                <NavDropdown id="drop-down" style={{color:"white",marginRight:"10px",fontSize:"20px"}} title="수강신청 설명">
+                                    <NavDropdown.Item style={{color:"#162B7D"}} href="#action/3.1">수강신청 튜토리얼</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item style={{color:"#162B7D"}} href="#action/3.2">수강신청 설명</NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link style={{color:"white", marginRight:"10px",fontSize:"20px"}} href="#link">수강신청 FAQ</Nav.Link>
+                                <Nav.Link style={{color:"white", marginRight:"10px",fontSize:"20px"}} href="#link">수강신청 Q&A</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        )
+    }
+}
+export default Navbars
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbars from "../navbar/Navbar";
 class Main extends React.Component {
     render() {
         return(
             <div>
+                <Navbars></Navbars>
                 <h1>
                     Main
                 </h1>
