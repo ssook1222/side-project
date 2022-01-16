7a0a310be88f50b49c478e60ebacfec7fe1c2c6c
ssook1222 <chlthr1222@naver.com>
2022-01-16 18:30:11 +0900

    UPDATE : 이미지 추가, 백그라운드 앞부분 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbars from "../navbar/Navbar";
+import {Image} from "react-bootstrap";
 class Main extends React.Component {
     render() {
         return(
             <div>
                 <Navbars></Navbars>
-                <h1>
-                    Main
-                </h1>
+                <div className="background"
+                     style={{width:"100%",
+                         marginTop:"-20px",
+                         height:"500px",
+                         backgroundColor:"#162B7D"}}>
+                    <Image
+                        style={{display:"block",marginRight:"auto",marginLeft:"auto",marginBottom:"10px",width:"75%"}}
+                        src="../images/Main_Text_Logo.png"></Image>
+                </div>
             </div>
         )
     }
