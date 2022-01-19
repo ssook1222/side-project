8d5aefc27cc66e5cadc846debbe69a9d95e50f0a
ssook1222 <chlthr1222@naver.com>
2022-01-19 20:02:35 +0900

    UPDATE : 네비게이션 바 변경 및 footer 추가

---
+++ src/footer/FooterMain.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Footer.css";
+
+class FooterMain extends React.Component {
+    render() {
+        return(
+            <div className="footer">
+                <div className="footer-text">
+                <h4>Contact US</h4>
+                <h5><a className="mail" href="mailto:sw18@sookmyung.ac.kr">sw18@sookmyung.ac.kr</a></h5>
+                <h5><a className="mail" href="mailto:chlthr1222@sookmyung.ac.kr">chlthr1222@sookmyung.ac.kr</a></h5>
+                <hr/>
+                <h5>본 사이트의 컨텐츠는 저작권법의 보호를 받는 바, 무단 전재,복사,배포 등을 금합니다.</h5>
+                <h6>Copyright © 2022 WiseCourseRegistration. All Rights Reserved.</h6>
+            </div>
+            </div>
+        )
+    }
+}
+export default FooterMain
