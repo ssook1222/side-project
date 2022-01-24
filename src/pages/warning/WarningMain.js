1094e64a2657a710409ebc473e47f1e3423f31c2
ssook <60427387+ssook1222@users.noreply.github.com>
2022-01-24 17:13:27 +0900

    [Feature/warning] UPDATE : 수강신청 페이지 1차 완료 (#22)
    * ADD : 신청 주의사항 페이지 기본 틀 생성
    
    * UPDATE : 테이블 추가
    
    * UPDATE : 수강신청 일정 설명 페이지 1차 완료

--- src/pages/warning/WarningMain.js
+++ src/pages/warning/WarningMain.js
@@ -3,12 +3,18 @@ import Navbars from "../../navbar/Navbar";
 import Footers from  "../../footer/FooterMain";
 import Schedule from "./Schedule";
 import Information from "./Information";
+import {Image} from "react-bootstrap";
 
 class WarningMain extends React.Component {
     render() {
         return(
             <div>
                 <Navbars></Navbars>
+                <div className="background">
+                    <Image
+                        className="img"
+                        src="../images/Schedule.png"></Image>
+                </div>
                 <Schedule></Schedule>
                 <Information></Information>
                 <Footers></Footers>
