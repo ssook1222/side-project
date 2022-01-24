a2db29a89687a9771ecc93886085df9d5f827a16
ssook1222 <chlthr1222@naver.com>
2022-01-24 16:34:50 +0900

    UPDATE : 테이블 추가

--- src/pages/warning/Schedule.js
+++ src/pages/warning/Schedule.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Warning.css";
+import Tables from "./Table";
 
 class Schedule extends React.Component {
     render() {
         return(
             <div>
                 <h3 className="title">2022-1학기 수강신청 일정</h3>
+                <Tables></Tables>
             </div>
         )
     }
