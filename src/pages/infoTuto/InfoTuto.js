425a89b7213d07c281fbb0007ce511099da7339c
1010Min <61402459+1010Min@users.noreply.github.com>
2022-01-30 04:12:31 +0900

    [Feature/info] UPDATE : 수강신청 설명 튜토리얼 추가 (#27)
    Co-authored-by: ssook <60427387+ssook1222@users.noreply.github.com>

--- src/pages/infoTuto/InfoTuto.js
+++ src/pages/infoTuto/InfoTuto.js
@@ -8,6 +8,15 @@ class InfoTuto extends React.Component {
     render() {
           const popoverBtn2 = (
             <Popover id="popover-basic">
+              <Popover.Header as="h3" id = "boldTxt">수강순위 조회</Popover.Header>
+              <Popover.Body id = "justTxt">
+                    그것에 대한 내용...
+              </Popover.Body>
+            </Popover>
+          );
+
+          const popoverBtn3 = (
+            <Popover id="popover-basic">
               <Popover.Header as="h3" id = "boldTxt">수업평가결과조회</Popover.Header>
               <Popover.Body id = "justTxt">
                     과목별/교수별 이전 수업평가 결과 조회가 가능합니다.
@@ -35,38 +44,85 @@ class InfoTuto extends React.Component {
                 style={{fontSize: "1.3vmax", fontWeight: "bold", color: "#162B7D", marginLeft: "1%"}}>
                     * 버튼 혹은 검은색 물음표를 클릭하면 해당 내용에 대한 설명을 확인할 수 있습니다.
                 </h1>
+                
+                <Image style = {{width: "100%", marginBottom: "25px"}} src="../images/tutorial/0.png" />
 
-                <h1
-                style={{marginLeft: "1.5%"}}>
-                    개설과목조회.
-                    <OverlayTrigger
-                        placement="right"
-                        overlay={<Tooltip id="button-tooltip-2">
-                            <h1>개설과목조회</h1>
-                            <h2>그것에 대한 설명...</h2>
-                        </Tooltip>}>
-                        {({ ref, ...triggerHandler }) => (
-                        <Button
-                            variant="outline-light"
-                            style = {{padding: "0px", marginLeft: "10px"}}
-                            {...triggerHandler}
-                            className="d-inline-flex align-items-center">
-                            <Image
-                            ref={ref}
-                            style = {{width:"25px"}}
-                            src="../images/tutorial/question.png"/>
-                        </Button>
-                        )}
+                <div className='registerList'>
+                    <h4 style={{marginTop: "25px", marginLeft: "1.5%", fontSize: "1.7vw" }}>학생기본정보.</h4>
+
+                    <ColoredLine color="#387EC6" />
+                    
+                    <Image id="tutoImg" src="../images/tutorial/1.png" />
+                </div>
+
+                <div className='registerList'>
+                    <h4
+                    style={{marginLeft: "1.5%", fontSize: "1.7vw"}}>
+                        수강신청내역.
+                        <OverlayTrigger
+                            placement="right"
+                            overlay={<Tooltip id="button-tooltip-2">
+                                <h5>수강신청내역</h5>
+                                <h6>그것에 대한 설명...</h6>
+                            </Tooltip>}>
+                            {({ ref, ...triggerHandler }) => (
+                            <Button
+                                variant="outline-light"
+                                style = {{padding: "0px", marginLeft: "10px"}}
+                                {...triggerHandler}
+                                className="d-inline-flex align-items-center">
+                                <Image
+                                ref={ref}
+                                style = {{width:"25px"}}
+                                src="../images/tutorial/question.png"/>
+                            </Button>
+                            )}
+                        </OverlayTrigger>
+                        
+                    </h4>
+
+                    <ColoredLine color="#387EC6" />
+                    
+                    <OverlayTrigger trigger="click" placement="right" overlay={popoverBtn2}>
+                        <Button variant="success" id="tutoBtn">수강순위 조회</Button>
                     </OverlayTrigger>
-                </h1>
 
-                <ColoredLine color="#387EC6" />
+                    <Image id="tutoImg" src="../images/tutorial/2.png" />
+                </div>
 
-                <OverlayTrigger trigger="click" placement="right" overlay={popoverBtn2}>
-                    <Button variant="success" id="tutoBtn">수업평가결과조회</Button>
-                </OverlayTrigger>
+                <div className='classSearch'>
+                    <h4
+                    style={{marginLeft: "1.5%", fontSize: "1.7vw" }}>
+                        개설과목조회.
+                        <OverlayTrigger
+                            placement="right"
+                            overlay={<Tooltip id="button-tooltip-2">
+                                <h5>개설과목조회</h5>
+                                <h6>그것에 대한 설명...</h6>
+                            </Tooltip>}>
+                            {({ ref, ...triggerHandler }) => (
+                            <Button
+                                variant="outline-light"
+                                style = {{padding: "0px", marginLeft: "10px"}}
+                                {...triggerHandler}
+                                className="d-inline-flex align-items-center">
+                                <Image
+                                ref={ref}
+                                style = {{width:"25px"}}
+                                src="../images/tutorial/question.png"/>
+                            </Button>
+                            )}
+                        </OverlayTrigger>
+                    </h4>
 
-                <Image id="tutoImg" src="../images/tutorial/2.png" />
+                    <ColoredLine color="#387EC6" />
+
+                    <OverlayTrigger trigger="click" placement="right" overlay={popoverBtn3}>
+                        <Button variant="success" id="tutoBtn">수업평가결과조회</Button>
+                    </OverlayTrigger>
+
+                    <Image id="tutoImg" src="../images/tutorial/3.png" />
+                </div>
 
                 <Footers></Footers>
             </div>
