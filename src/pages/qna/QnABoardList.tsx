2ed67db5a9298f0951804fc31a1867e18fb68ce5
ssook <60427387+ssook1222@users.noreply.github.com>
2022-02-05 08:31:41 +0900

    [Feature/qna] UPDATE : 프론트엔드 + 백엔드 연동 (#32)
    * UPDATE : 프론트엔드 + 백엔드 연동

--- src/pages/qna/QnABoardList.tsx
+++ src/pages/qna/QnABoardList.tsx
@@ -1,10 +1,36 @@
 // @ts-ignore
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Button, Col, ListGroup, Row} from "react-bootstrap";
+import {Board} from "../../dto/Board";
 
-const BoardList = () => {
+const BoardList: React.FC = () => {
+    const [boardList, setBoardList] = useState<Array<Board>>([]);
+
+    useEffect(() => {
+        getBoardList();
+    }, []);
+
+    const getBoardList = async () => {
+        const res  = await axios.get('/api/board/read?page_number=1&page_size=10');
+        console.log(res);
+        setBoardList(res.data);
+    }
+
     return (
         <div>
-
+            {
+                boardList.map((board: Board)=>
+                        <Row style={{
+                            paddingBottom:"10px",paddingTop:"10px",
+                            borderBottom:"1px solid #dddddd"}}>
+                            <Button className="btn-light">
+                                <Col style={{display:"inline-block"}} xs={12} md={8}>{board.title}</Col>
+                                <Col style={{display:"inline-block"}} xs={6} md={4}>{board.id}</Col>
+                            </Button>
+                        </Row>
+                    )
+            }
         </div>
     );
 };
