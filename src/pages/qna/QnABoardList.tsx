1c88dda42c0251db912f79d863f56bbe8ab611ad
ssook1222 <chlthr1222@naver.com>
2022-02-06 21:14:30 +0900

    UPDATE : QnA 작성 프론트엔드 틀만 추가

--- src/pages/qna/QnABoardList.tsx
+++ src/pages/qna/QnABoardList.tsx
@@ -4,7 +4,8 @@ import axios from "axios";
 import {Button, Col, ListGroup, Row} from "react-bootstrap";
 import {Board} from "../../dto/Board";
 
-const BoardList: React.FC = () => {
+const BoardList: React.FC = (props: any) => {
+    console.log(props);
     const [boardList, setBoardList] = useState<Array<Board>>([]);
 
     useEffect(() => {
@@ -12,13 +13,19 @@ const BoardList: React.FC = () => {
     }, []);
 
     const getBoardList = async () => {
-        const res  = await axios.get('/api/board/read?page_number=1&page_size=10');
-        console.log(res);
+        const res  = await axios.get(`/api/board/read?page_number=1&page_size=10`);
+        //생각해보니 시작할 때는 항상 1임
+        console.log(res.data.length);
         setBoardList(res.data);
     }
 
     return (
         <div>
+            <Row className="mb-3 justify-content-end">
+                <Col xs="auto" sm="auto">
+                    <Button variant="light" href="/board-register">등 록</Button>
+                </Col>
+            </Row>
             {
                 boardList.map((board: Board)=>
                         <Row style={{
