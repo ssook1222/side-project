8552944c1b9caffc04b0d308bddd7cc0b6293d99
ssook1222 <chlthr1222@naver.com>
2022-02-07 06:46:35 +0900

    UPDATE : html5 validation 추가

--- src/pages/qna/BoardRegister.tsx
+++ src/pages/qna/BoardRegister.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Form} from "react-bootstrap";
 
 const BoardRegister: React.FC = () => {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const form = event.currentTarget;
+        if (!form.checkValidity()) {
+            setValidated(false);
+            return;
+        }
+
+        setValidated(true);
+    };
+
     return (
-        <Form>
+        <Form validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId="titleInput">
                 <Form.Label>제목</Form.Label>
-                <Form.Control required type="email" placeholder="" />
+                <Form.Control required type="textarea" placeholder="20자 이내로 작성해주세요." maxLength={20} />
+                <Form.Control.Feedback>유효한 제목 값입니다.</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">유효하지 않은 제목 값입니다.</Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="contentText">
                 <Form.Label>내용</Form.Label>
-                <Form.Control required as="textarea" rows={20} />
+                <Form.Control required as="textarea" placeholder="100자 이내로 작성해주세요." maxLength={100} rows={20}/>
+                <Form.Control.Feedback>유효한 내용 값입니다.</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">유효하지 않은 내용 값입니다.</Form.Control.Feedback>
             </Form.Group>
             <Button variant="light" type="submit">
                 저장
