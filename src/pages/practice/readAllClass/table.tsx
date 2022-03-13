39e297595fd6241eb661ca02bca2b32dfbe4bd10
ssook1222 <chlthr1222@naver.com>
2022-03-14 06:45:26 +0900

    UPDATE : 프론트엔드 - 백엔드 연동 2

--- src/pages/practice/readAllClass/table.tsx
+++ src/pages/practice/readAllClass/table.tsx
@@ -1,38 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Table} from "../../../dto/Table";
+import {Button, Col, Row} from "react-bootstrap";
 
 
 const TableView: React.FC = () => {
 
-    const [table, setTable] = useState<Table>({
-        id: 0, lectureForm:" ", lectureName:" ",
-        classType: "",
-        dept: "",
-        difficulty: "",
-        lectureApply: 0,
-        lectureClassfi: "",
-        lectureLanguage: "",
-        lectureNumber: 0,
-        lecturePeople: 0,
-        lecturePoint: "",
-        lectureSeat: 0,
-        lectureSubClass: 0,
-        lectureTime: "",
-        lectureType: "",
-        lectureWarn: "",
-        movie: "",
-        pf: "",
-        plan: "",
-        professor: "",
-        schoolExchange: "",
-        snowboard: "",
-        specification1: "",
-        specification2: "",
-        specification3: "",
-        step: "",
-        withComp: ""
-    });
+    const [table, setTable] = useState<Array<Table>>([]);
 
     useEffect(() => {
         getTable();
@@ -49,7 +23,14 @@ const TableView: React.FC = () => {
         <div>
             <h4>plz</h4>
             {
-
+                table.map((table: Table)=>
+                    <Row style={{
+                        paddingBottom:"10px",paddingTop:"10px",
+                        borderBottom:"1px solid #dddddd"}}>
+                            <Col style={{display:"inline-block"}} xs={12} md={8}>{table.id}</Col>
+                            <Col style={{display:"inline-block"}} xs={6} md={4}>{table.lectureName}</Col>
+                    </Row>
+                )
             }
         </div>
     );
