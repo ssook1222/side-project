0b703b230b6880dba9f3586a19e564a1795e9888
ssook <60427387+ssook1222@users.noreply.github.com>
2022-03-23 06:01:45 +0900

    [Feature/practice] UPDATE : 파일 겹치는 것 리팩토링 및 상황별 컴포넌트 수정 (#63)
    * UPDATE : 테이블 별로 파일 생성 및 파일 이름 겹치는 것 리팩터링
    
    Co-authored-by: 1010Min <1010ally@naver.com>

--- src/pages/practice/readAllClass/ITTable.tsx
+++ src/pages/practice/readAllClass/ITTable.tsx
@@ -13,7 +13,7 @@ const TableView: React.FC = () => {
     }, []);
 
     const getTable = async () => {
-        const res = await axios.get(`/api/tables/jinro`);
+        const res = await axios.get(`/api/tables/it`);
         console.log(res.data);
         setTable(res.data);
     }
@@ -26,33 +26,33 @@ const TableView: React.FC = () => {
                     <Row style={{
                         paddingBottom:"10px",paddingTop:"10px",
                         borderBottom:"1px solid #dddddd"}}>
-                            <Col className="contents">{table.lectureForm}</Col>
-                            <Col className="contents">{table.lectureName}</Col>
-                            <Col className="contents">{table.lectureNumber}</Col>
-                            <Col className="contents">{table.lectureSubClass}</Col>
-                            <Col className="contents">{table.plan}</Col>
-                            <Col className="contents">{table.movie}</Col>
-                            <Col className="contents">{table.classType}</Col>
-                            <Col className="contents">{table.lectureTime}</Col>
-                            <Col className="contents">{table.professor}</Col>
-                            <Col className="contents">{table.step}</Col>
-                            <Col className="contents">{table.lecturePoint}</Col>
-                            <Col className="contents">{table.lecturePeople}</Col>
-                            <Col className="contents">{table.lectureApply}</Col>
-                            <Col className="contents">{table.lectureSeat}</Col>
-                            <Col className="contents">{table.lectureType}</Col>
-                            <Col className="contents">{table.lectureLanguage}</Col>
-                            <Col className="contents">{table.withComp}</Col>
-                            <Col className="contents">{table.lectureClassfi}</Col>
-                            <Col className="contents">{table.lectureWarn}</Col>
-                            <Col className="contents">{table.snowboard}</Col>
-                            <Col className="contents">{table.dept}</Col>
-                            <Col className="contents">{table.pf}</Col>
-                            <Col className="contents">{table.schoolExchange}</Col>
-                            <Col className="contents">{table.difficulty}</Col>
-                            <Col className="contents">{table.specification1}</Col>
-                            <Col className="contents">{table.specification2}</Col>
-                            <Col className="contents">{table.specification3}</Col>
+                        <Col className="contents">{table.lectureForm}</Col>
+                        <Col className="contents">{table.lectureName}</Col>
+                        <Col className="contents">{table.lectureNumber}</Col>
+                        <Col className="contents">{table.lectureSubClass}</Col>
+                        <Col className="contents">{table.plan}</Col>
+                        <Col className="contents">{table.movie}</Col>
+                        <Col className="contents">{table.classType}</Col>
+                        <Col className="contents">{table.lectureTime}</Col>
+                        <Col className="contents">{table.professor}</Col>
+                        <Col className="contents">{table.step}</Col>
+                        <Col className="contents">{table.lecturePoint}</Col>
+                        <Col className="contents">{table.lecturePeople}</Col>
+                        <Col className="contents">{table.lectureApply}</Col>
+                        <Col className="contents">{table.lectureSeat}</Col>
+                        <Col className="contents">{table.lectureType}</Col>
+                        <Col className="contents">{table.lectureLanguage}</Col>
+                        <Col className="contents">{table.withComp}</Col>
+                        <Col className="contents">{table.lectureClassfi}</Col>
+                        <Col className="contents">{table.lectureWarn}</Col>
+                        <Col className="contents">{table.snowboard}</Col>
+                        <Col className="contents">{table.dept}</Col>
+                        <Col className="contents">{table.pf}</Col>
+                        <Col className="contents">{table.schoolExchange}</Col>
+                        <Col className="contents">{table.difficulty}</Col>
+                        <Col className="contents">{table.specification1}</Col>
+                        <Col className="contents">{table.specification2}</Col>
+                        <Col className="contents">{table.specification3}</Col>
                     </Row>
                 )
             }
